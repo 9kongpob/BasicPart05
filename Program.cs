d0588ff4650369b6e46c019e5164441c93b16474
IT4 <IT4@siwt.co.th>
2019-11-14 09:04:29 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicPart05_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+
+            //for
+
+            //do..While
+
+            Console.ReadKey();
+        }
+    }
+}
